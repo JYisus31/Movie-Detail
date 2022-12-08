a231584961e68a2506b8e6a0612de2840792d469
Juan Jesus Rico Juarez <17030649@itcelaya.edu.mx>
2022-12-08 03:36:16 -0600

    commit final movies 2

---
+++ lib/src/database/database_helper.dart
@@ -0,0 +1,154 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import '../models/notas_model.dart';
+import '../models/popular_movie_model.dart';
+import '../models/profile_Model.dart';
+
+class DatabaseHelper {
+  static final _DBName = "NOTASBD";
+  static final _DBVersion = 1;
+  static final _TBLNotes = "tblNotas";
+  static final _TBLProfile = "tblProfile";
+  static final _TBLFavMovies = "tblFavMovies";
+
+  static Database? _database;
+
+  Future<Database?> get database async {
+    if (_database != null) return _database;
+    _database = await _initDatabase();
+    return _database;
+  }
+
+  Future<Database> _initDatabase() async {
+    Directory carpeta = await getApplicationDocumentsDirectory();
+    // String rutaBD = "$carpeta.path/$_DBName";
+    String rutaBD = join(carpeta.path, _DBName);
+    return openDatabase(
+      rutaBD,
+      version: _DBVersion,
+      onCreate: _createTable,
+      onUpgrade: _onUpgrade,
+      onDowngrade: _onDowngrade,
+    );
+  }
+
+  Future<void> _createTable(Database db, int version) async {
+    String script =
+        "CREATE TABLE $_TBLNotes (id INTEGER PRIMARY KEY, titulo VARCHAR(50), detalle VARCHAR(100))";
+    await db.execute(script);
+
+    script =
+        "CREATE TABLE $_TBLProfile (id INTEGER PRIMARY KEY, name VARCHAR(30), aPaterno VARCHAR(50), aMaterno VARCHAR(50), phoneNumber INT(10), email VARCHAR(50), image VARCHAR(250))";
+    await db.execute(script);
+
+    script = '''CREATE TABLE $_TBLFavMovies (
+        id INTEGER PRIMARY KEY,
+        backdrop_path VARCHAR(250),
+        original_language VARCHAR(50),
+        original_title VARCHAR(50),
+        overview VARCHAR(250),
+        popularity VARCHAR(50),
+        poster_path VARCHAR(250),
+        title VARCHAR(50),
+        vote_average VARCHAR(50))''';
+    await db.execute(script);
+  }
+
+  Future<int> insert(Map<String, dynamic> row) async {
+    var connection = await database;
+    return connection!.insert(_TBLNotes, row);
+  }
+
+  Future<int> update(Map<String, dynamic> row) async {
+    var connection = await database;
+    return connection!
+        .update(_TBLNotes, row, where: "id = ?", whereArgs: [row['id']]);
+  }
+
+  Future<int> delete(int id) async {
+    var connection = await database;
+    return connection!.delete(_TBLNotes, where: "id = ?", whereArgs: [id]);
+  }
+
+  Future<List<NotasModel>> getAllNotes() async {
+    var connection = await database;
+    var notesList = await connection!.query(_TBLNotes);
+    return notesList.map((note) => NotasModel.fromMap(note)).toList();
+  }
+
+  Future<NotasModel> getNote(int id) async {
+    var connection = await database;
+    var note =
+        await connection!.query(_TBLNotes, where: "id = ?", whereArgs: [id]);
+    return NotasModel.fromMap(note.first);
+  }
+
+  FutureOr<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
+    print("Upgrading...");
+    // Reestructuracion de la BD
+    String script =
+        "CREATE TABLE $_TBLProfile (id INTEGER PRIMARY KEY, name VARCHAR(30), aPaterno VARCHAR(50), aMaterno VARCHAR(50), phoneNumber INT(10), email VARCHAR(50), image VARCHAR(250))";
+    await db.execute(script);
+  }
+
+  FutureOr<void> _onDowngrade(
+      Database db, int oldVersion, int newVersion) async {
+    print("Downgrading...");
+    String script = "DROP TABLE $_TBLProfile";
+    await db.execute(script);
+  }
+
+  Future<ProfileModel?> getUser() async {
+    var connection = await database;
+    var userData = await connection!.query(_TBLProfile);
+    if (userData.length > 0) {
+      return ProfileModel.fromMap(userData.first);
+    }
+    return null;
+  }
+
+  Future<int> insertUserInfo(Map<String, dynamic> row) async {
+    var connection = await database;
+    return connection!.insert(_TBLProfile, row);
+  }
+
+  Future<int> updateUser(Map<String, dynamic> row) async {
+    var connection = await database;
+    return connection!
+        .update(_TBLProfile, row, where: "id = ?", whereArgs: [row['id']]);
+  }
+
+  Future<List<PopularMoviesModel>?> getFavoritesMovies() async {
+    var connection = await database;
+    var moviesList = await connection!.query(_TBLFavMovies);
+    return moviesList
+        .map((movie) => PopularMoviesModel.fromMap(movie))
+        .toList();
+  }
+
+  Future<int> addToFavorites(Map<String, dynamic> row) async {
+    var connection = await database;
+    print(row);
+    print("Entre");
+    return connection!.insert(_TBLFavMovies, row);
+  }
+
+  Future<int> removeFromFavorites(int id) async {
+    var connection = await database;
+    return connection!.delete(_TBLFavMovies, where: "id = ?", whereArgs: [id]);
+  }
+
+  Future<bool> seekInFavorites(int id) async {
+    var connection = await database;
+    var favorite = await connection!
+        .query(_TBLFavMovies, where: "id = ?", whereArgs: [id]);
+    if (favorite.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
---
+++ lib/src/screens/popular_screen.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:popular_movie/src/Network/api_popular.dart';
+
+import 'package:popular_movie/src/database/database_helper.dart';
+import 'package:popular_movie/src/models/popular_movie_model.dart';
+import 'package:popular_movie/src/views/card_popular.dart';
+
+class PopularScreen extends StatefulWidget {
+  PopularScreen({Key? key}) : super(key: key);
+
+  @override
+  _PopularScreen createState() => _PopularScreen();
+}
+
+class _PopularScreen extends State<PopularScreen> {
+  late DatabaseHelper _databaseHelper;
+  Api? apiPopular;
+  var tabIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    apiPopular = Api();
+    _databaseHelper = DatabaseHelper();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    void _upDate() {
+      setState(() {});
+    }
+
+    final _tabScreens = <Widget>[
+      _buildPopularMovies(),
+      _buildFavoriteMovies(_upDate),
+    ];
+
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            tabIndex = index;
+          });
+        },
+        currentIndex: tabIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie_filter_rounded),
+            label: 'Popular',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_rounded),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+      appBar: AppBar(title: Text('Movies from API')),
+      body: _tabScreens[tabIndex],
+    );
+  }
+
+  Widget _buildFavoriteMovies(Function _upDate) {
+    return FutureBuilder(
+      future: _databaseHelper.getFavoritesMovies(),
+      builder: (BuildContext context,
+          AsyncSnapshot<List<PopularMoviesModel>?> snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Error: ${snapshot.error}'),
+          );
+        } else {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return snapshot.data!.length != 0
+                ? _listPopularMovies(snapshot.data, _upDate)
+                : Center(
+                    child: Text('No favorites movies'),
+                  );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        }
+      },
+    );
+  }
+
+  Widget _buildPopularMovies() {
+    return FutureBuilder(
+        future: apiPopular!.getAllPopularMovies(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<dynamic>?> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error'),
+            );
+          } else {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return _listPopularMovies(snapshot.data, null);
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }
+        });
+  }
+
+  Widget _listPopularMovies(List<dynamic>? data, Function? _upDate) {
+    return ListView.separated(
+      itemBuilder: (context, index) {
+        PopularMoviesModel popularMoviesList = data![index];
+        return CardCardPopularView(popular: popularMoviesList, onTap: _upDate);
+      },
+      separatorBuilder: (_, __) => Divider(
+        height: 10,
+      ),
+      itemCount: data!.length,
+    );
+  }
+}
